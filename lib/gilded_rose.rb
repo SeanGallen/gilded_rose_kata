1e7aa22d8e0435a1dd9d6b0edb700fab7a95b47f
Josh Cheek <josh.cheek@gmail.com>
2016-04-18 11:38:07 -0500

    Apply the Open Closed principle

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,49 +1,102 @@
 def update_quality(items)
-  items.each do |item|
-    update_item item
+  items.each { |item| AgeItem.call item }
+end
+
+class AgeItem
+  # You can extend functionality by adding agers
+  @agers = {}
+  def self.agers
+    @agers
+  end
+
+  def self.call(item)
+    agers[item.name].new(item).age
+  end
+
+  # AgeItem is a superclass for other agers, they can override as appropriate
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def delta_sell_in
+    -1
+  end
+
+  def delta_quality
+    raise 'implement me'
+  end
+
+  def max_quality
+    50
+  end
+
+  def min_quality
+    0
+  end
+
+  def age
+    item.sell_in += delta_sell_in
+    item.quality += delta_quality
+    item.quality = max_quality if max_quality  < item.quality
+    item.quality = min_quality if item.quality < min_quality
   end
 end
 
-def update_item(item)
-  if item.name == 'Aged Brie'
-    item.sell_in -= 1
-    if item.sell_in < 0
-      item.quality += 2
-    else
-      item.quality += 1
-    end
-    item.quality = 50 if 50 < item.quality
-    item.quality = 0  if item.quality < 0
-  elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
-    item.sell_in -= 1
-    item.quality += 1
-    if item.sell_in < 5
-      item.quality += 2
-    elsif item.sell_in < 10
-      item.quality += 1
-    end
-    item.quality = 50 if 50 < item.quality
-    item.quality = 0  if item.sell_in < 0
-  elsif item.name == 'Sulfuras, Hand of Ragnaros'
-  elsif item.name == "NORMAL ITEM"
-    item.sell_in -= 1
-    if item.sell_in < 0
-      item.quality -= 2
-    else
-      item.quality -= 1
-    end
-    item.quality = 0 if item.quality < 0
-  elsif item.name == "Conjured Mana Cake"
-    item.sell_in -= 1
-    if 0 < item.sell_in
-      item.quality -= 2
-    else
-      item.quality -= 4
-    end
-    item.quality = 0 if item.quality < 0
-  else
-    raise item.name
+class AgeItem::AgedBrie < AgeItem
+  def delta_quality
+    item.sell_in < 0 ? 2 : 1
   end
+end
+
+
+class AgeItem::BackstagePass < AgeItem
+  def delta_quality
+    return -item.quality if item.sell_in < 0
+    return 3             if item.sell_in < 5
+    return 2             if item.sell_in < 10
+    return 1
+  end
+end
+
+
+class AgeItem::Sulfuras < AgeItem
+  def delta_sell_in
+    0
+  end
+  def delta_quality
+    0
+  end
+  def max_quality
+    item.quality
+  end
+  def min_quality
+    item.quality
+  end
+end
+
+
+class AgeItem::Normal < AgeItem
+  def delta_quality
+    item.sell_in < 0 ? -2 : -1
+  end
+end
+
+
+class AgeItem::ConjuredMana < AgeItem
+  def delta_quality
+    0 < item.sell_in ? -2 : -4
+  end
+end
+
+
+class AgeItem
+  agers['Aged Brie']                                 = AgedBrie
+  agers['Backstage passes to a TAFKAL80ETC concert'] = BackstagePass
+  agers['Sulfuras, Hand of Ragnaros']                = Sulfuras
+  agers['NORMAL ITEM']                               = Normal
+  agers['Conjured Mana Cake']                        = ConjuredMana
 end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
